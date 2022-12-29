25fc464a9b249160eade8e1f4d2deb71dffa66df
Mageia <yzg963@gmail.com>
2022-12-29 11:33:51 +0800

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Counter from "./Counter.svelte";
+  import welcome from "$lib/images/svelte-welcome.webp";
+  import welcome_fallback from "$lib/images/svelte-welcome.png";
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+  <p class="text-teal-500">Tailwind is working</p>
+
+  <Counter />
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 0.6;
+  }
+</style>
